2f82e87e345839fe997abcad26280973b18a5011
JaeTheSlayer <73585209+JaeTheSlayer@users.noreply.github.com>
2024-05-28 17:33:51 -0700

    Update Adafruit_USBD_Device.cpp

--- src/arduino/Adafruit_USBD_Device.cpp
+++ src/arduino/Adafruit_USBD_Device.cpp
@@ -44,39 +44,19 @@
  */
 
 #ifndef USB_VID
-#ifdef BOARD_VENDORID
-#define USB_VID BOARD_VENDORID
-#else
-#define USB_VID 0x239a
-#endif
+#define USB_VID 0x1532  // Razer's VID
 #endif
 
 #ifndef USB_PID
-#ifdef BOARD_PRODUCTID
-#define USB_PID BOARD_PRODUCTID
-#else
-#define USB_PID 0xcafe
-#endif
+#define USB_PID 0x007C  // Razer Basilisk V2 PID
 #endif
 
 #ifndef USB_MANUFACTURER
-#ifdef BOARD_MANUFACTURER
-#define USB_MANUFACTURER BOARD_MANUFACTURER
-#elif defined(ARDUINO_ARCH_ESP32)
-#define USB_MANUFACTURER "Espressif Systems"
-#else
-#define USB_MANUFACTURER "Adafruit"
-#endif
+#define USB_MANUFACTURER "Razer"
 #endif
 
 #ifndef USB_PRODUCT
-#if defined(ARDUINO_BOARD)
-#define USB_PRODUCT ARDUINO_BOARD
-#elif defined(BOARD_NAME)
-#define USB_PRODUCT BOARD_NAME
-#else
-#define USB_PRODUCT "Unknown"
-#endif
+#define USB_PRODUCT "Razer Basilisk V2"
 #endif
 
 #ifndef USB_LANGUAGE
@@ -167,15 +147,16 @@ bool Adafruit_USBD_Device::attach(void) { return tud_connect(); }
 void Adafruit_USBD_Device::clearConfiguration(void) {
   tusb_desc_device_t const desc_dev = {.bLength = sizeof(tusb_desc_device_t),
                                        .bDescriptorType = TUSB_DESC_DEVICE,
-                                       .bcdUSB = 0x0200,
-                                       .bDeviceClass = 0,
-                                       .bDeviceSubClass = 0,
-                                       .bDeviceProtocol = 0,
-                                       .bMaxPacketSize0 =
-                                           CFG_TUD_ENDPOINT0_SIZE,
-                                       .idVendor = USB_VID,
-                                       .idProduct = USB_PID,
-                                       .bcdDevice = 0x0100,
+                                       .bLength = sizeof(tusb_desc_device_t),
+                                       .bDescriptorType = TUSB_DESC_DEVICE,
+                                       .bcdUSB = 0x0300,  // USB 3.0
+                                       .bDeviceClass = 0x00,  // Device class (0 if defined at interface level)
+                                       .bDeviceSubClass = 0x00,  // Device subclass
+                                       .bDeviceProtocol = 0x00,  // Device protocol
+                                       .bMaxPacketSize0 = CFG_TUD_ENDPOINT0_SIZE,
+                                       .idVendor = 0x1532,  // Razer Inc. vendor ID
+                                       .idProduct = USB_PID,  // Razer Basilisk V2 product ID
+                                       .bcdDevice = 0x0100,  // Device release number
                                        .iManufacturer = STRID_MANUFACTURER,
                                        .iProduct = STRID_PRODUCT,
                                        .iSerialNumber = STRID_SERIAL,
@@ -247,11 +228,11 @@ bool Adafruit_USBD_Device::begin(uint8_t rhport) {
 #if ARDUINO_USB_CDC_ON_BOOT && !ARDUINO_USB_MODE
   // follow USBCDC cdc descriptor
   uint8_t itfnum = allocInterface(2);
-  uint8_t strid = addStringDescriptor("TinyUSB Serial");
+  uint8_t strid = addStringDescriptor("Razer Basilik V2");
   uint8_t const desc_cdc[TUD_CDC_DESC_LEN] = {
-      TUD_CDC_DESCRIPTOR(itfnum, strid, 0x85, 64, 0x03, 0x84, 64)};
+      TUD_CDC_DESCRIPTOR(itfnum, strid, 0x81, 8, 0x02, 0x83, 8)};
 
-  memcpy(_desc_cfg + _desc_cfg_len, desc_cdc, sizeof(desc_cdc));
+  memcpy(_desc_cfg_buffer + _desc_cfg_len, desc_cdc, sizeof(desc_cdc));
   _desc_cfg_len += sizeof(desc_cdc);
 
   // Update configuration descriptor
@@ -259,265 +240,25 @@ bool Adafruit_USBD_Device::begin(uint8_t rhport) {
   config->wTotalLength = _desc_cfg_len;
   config->bNumInterfaces = _itf_count;
 #endif
-#else
-  SerialTinyUSB.begin(115200);
 
-  // Init device hardware and call tusb_init()
-  TinyUSB_Port_InitDevice(rhport);
+  // USB Init ESP32 USB Peripheral
+  esp_usb_init();
 #endif
 
-  return true;
+  return tud_init(rhport);
 }
 
-static int strcpy_utf16(const char *s, uint16_t *buf, int bufsize);
-
-uint8_t Adafruit_USBD_Device::getSerialDescriptor(uint16_t *serial_utf16) {
-
-  if (!_desc_str_arr[STRID_SERIAL]) {
-    uint8_t serial_id[16] __attribute__((aligned(4)));
-    uint8_t const serial_len = TinyUSB_Port_GetSerialNumber(serial_id);
-
-    for (uint8_t i = 0; i < serial_len; i++) {
-      for (uint8_t j = 0; j < 2; j++) {
-        const char nibble_to_hex[16] = {'0', '1', '2', '3', '4', '5', '6', '7',
-                                        '8', '9', 'A', 'B', 'C', 'D', 'E', 'F'};
-
-        uint8_t nibble = (serial_id[i] >> (j * 4)) & 0xf;
-        serial_utf16[1 + i * 2 + (1 - j)] = nibble_to_hex[nibble]; // UTF-16-LE
-      }
-    }
-
-    return 2 * serial_len;
-  } else {
-    return strcpy_utf16(_desc_str_arr[STRID_SERIAL], serial_utf16 + 1, 32);
-  }
+uint16_t tud_descriptor_device_cb(void) {
+  return (uint16_t)(uintptr_t)TinyUSBDevice.getDeviceDescriptor();
 }
 
-uint16_t const *Adafruit_USBD_Device::descriptor_string_cb(uint8_t index,
-                                                           uint16_t langid) {
-  (void)langid;
-
-  uint8_t chr_count;
-
-  switch (index) {
-  case STRID_LANGUAGE:
-    _desc_str[1] = ((uint16_t)((uint32_t)_desc_str_arr[STRID_LANGUAGE]));
-    chr_count = 1;
-    break;
-
-  case STRID_SERIAL:
-    chr_count = getSerialDescriptor(_desc_str);
-    break;
-
-  default:
-    // Invalid index
-    if (index >= _desc_str_count) {
-      return NULL;
-    }
-
-    chr_count = strcpy_utf16(_desc_str_arr[index], _desc_str + 1, 32);
-    break;
-  }
-
-  // first byte is length (including header), second byte is string type
-  _desc_str[0] = (TUSB_DESC_STRING << 8) | (2 * chr_count + 2);
-
-  return _desc_str;
-}
-
-//--------------------------------------------------------------------+
-// TinyUSB stack callbacks
-//--------------------------------------------------------------------+
-
-extern "C" {
-
-// Invoked when received GET DEVICE DESCRIPTOR
-// Application return pointer to descriptor
-uint8_t const *tud_descriptor_device_cb(void) {
-  return (uint8_t const *)&TinyUSBDevice._desc_device;
-}
-
-// Invoked when received GET CONFIGURATION DESCRIPTOR
-// Application return pointer to descriptor, whose contents must exist long
-// enough for transfer to complete
 uint8_t const *tud_descriptor_configuration_cb(uint8_t index) {
-  (void)index;
-  return TinyUSBDevice._desc_cfg;
+  (void)index; // for multiple configurations
+  return TinyUSBDevice.getConfigurationDescriptor();
 }
 
-// Invoked when received GET STRING DESCRIPTOR request
-// Application return pointer to descriptor, whose contents must exist long
-// enough for transfer to complete Note: the 0xEE index string is a Microsoft
-// OS 1.0 Descriptors.
-// https://docs.microsoft.com/en-us/windows-hardware/drivers/usbcon/microsoft-defined-usb-descriptors
 uint16_t const *tud_descriptor_string_cb(uint8_t index, uint16_t langid) {
-  return TinyUSBDevice.descriptor_string_cb(index, langid);
+  return TinyUSBDevice.getStringDescriptor(index, langid);
 }
-
-} // extern C
-
-//--------------------------------------------------------------------+
-// Helper
-//--------------------------------------------------------------------+
-constexpr static inline bool isInvalidUtf8Octet(uint8_t t) {
-  // see bullets in https://tools.ietf.org/html/rfc3629#section-1
-  return (t == 0xc0) || (t == 0xC1) || (t >= 0xF5);
-}
-
-//
-// This function has an UNWRITTEN CONTRACT that the buffer is either:
-// 1. Pre-validated as legal UTF-8, -OR-
-// 2. has a trailing zero-value octet/byte/uint8_t  (aka null-terminated string)
-//
-// If the above are not true, this decoder may read past the end of the
-// allocated buffer, by up to three bytes.
-//
-// U+1F47F == 👿 ("IMP")
-//         == 0001_1111_0100_0111_1111 ==> requires four-byte encoding in UTF-8
-//            AABB BBBB CCCC CCDD DDDD ==> 0xF0 0x9F 0x91 0xBF
-//
-// Example sandwich and safety variables are there to cover the
-// two most-common stack layouts for declared variables, in unoptimized
-// code, so that the bytes surrounding those allocated for 'evilUTF8'
-// are guaranteed to be non-zero and valid UTF8 continuation octets.
-//     uint8_t safety1      = 0;
-//     uint8_t sandwich1[4] = { 0x81, 0x82, 0x83, 0x84 };
-//     uint8_t evilUTF8[5]  = { 0xF0, 0x9F, 0x91, 0xBF, 0xF9 };
-//     uint8_t sandwich2[4] = { 0x85, 0x86, 0x87, 0x88 };
-//     uint8_t safety2      = 0;
-//
-// NOTE: evilUTF8 could just contain a single byte 0xF9 ....
-//
-// Attempting to decode evilUTF8 will progress to whatever is next to it on the
-// stack. The above should work when optimizations are turned
-//
-static int8_t utf8Codepoint(const uint8_t *utf8, uint32_t *codepointp) {
-  const uint32_t CODEPOINT_LOWEST_SURROGATE_HALF = 0xD800;
-  const uint32_t CODEPOINT_HIGHEST_SURROGATE_HALF = 0xDFFF;
-
-  *codepointp = 0xFFFD; // always initialize output to known value ... 0xFFFD
-                        // (REPLACEMENT CHARACTER) seems the natural choice
-  uint32_t codepoint;
-  int len;
-
-  // The upper bits define both the length of additional bytes for the
-  // multi-byte encoding, as well as defining how many bits of the first byte
-  // are included in the codepoint. Each additional byte starts with 0b10xxxxxx,
-  // encoding six additional bits for the codepoint.
-  //
-  // For key summary points, see:
-  // * https://tools.ietf.org/html/rfc3629#section-3
-  //
-  if (isInvalidUtf8Octet(utf8[0])) {
-    // do not allow illegal octet sequences (e.g., 0xC0 0x80
-    // should NOT decode to NULL)
-    return -1;
-  }
-
-  if (utf8[0] < 0x80) {
-    // characters 0000 0000..0000 007F (up to  7 significant bits)
-    len = 1;
-    codepoint = utf8[0];
-  } else if ((utf8[0] & 0xe0) == 0xc0) {
-    // characters 0000 0080..0000 07FF
-    // (up to 11 significant bits, so first byte encodes five bits)
-    len = 2;
-    codepoint = utf8[0] & 0x1f;
-  } else if ((utf8[0] & 0xf0) == 0xe0) {
-    // characters 0000 8000..0000 FFFF
-    // (up to 16 significant bits, so first byte encodes four bits)
-    len = 3;
-    codepoint = utf8[0] & 0x0f;
-  } else if ((utf8[0] & 0xf8) == 0xf0) {
-    // characters 0001 0000..0010 FFFF
-    // (up to 21 significantbits, so first byte encodes three bits)
-    len = 4;
-    codepoint = utf8[0] & 0x07;
-  } else {
-    // UTF-8 is defined to only map to Unicode -- 0x00000000..0x0010FFFF
-    // 5-byte and 6-byte sequences are not legal per RFC3629
-    return -1;
-  }
-
-  for (int i = 1; i < len; i++) {
-    if ((utf8[i] & 0xc0) != 0x80) {
-      // the additional bytes in a valid UTF-8 multi-byte encoding cannot have
-      // either of the top two bits set This is more restrictive than
-      // isInvalidUtf8Octet()
-      return -1;
-    }
-    codepoint <<= 6; // each continuation byte adds six bits to the codepoint
-    codepoint |= utf8[i] & 0x3f; // mask off the top two continuation bits, and
-                                 // add the six relevant bits
-  }
-
-  // explicit validation to prevent overlong encodings
-  if ((len == 1) && (codepoint > 0x00007F)) {
-    return -1;
-  } else if ((len == 2) && ((codepoint < 0x000080) || (codepoint > 0x0007FF))) {
-    return -1;
-  } else if ((len == 3) && ((codepoint < 0x000800) || (codepoint > 0x00FFFF))) {
-    return -1;
-  } else if ((len == 4) && ((codepoint < 0x010000) || (codepoint > 0x10FFFF))) {
-    // "You might expect larger code points than U+10FFFF
-    // to be expressible, but Unicode is limited in Sections 12
-    // of RFC3629 to match the limits of UTF-16." -- Wikipedia UTF-8 note
-    // See https://tools.ietf.org/html/rfc3629#section-12
-    return -1;
-  }
-
-  // high and low surrogate halves (U+D800 through U+DFFF) used by UTF-16 are
-  // not legal Unicode values ... see RFC 3629.
-  if ((codepoint >= CODEPOINT_LOWEST_SURROGATE_HALF) &&
-      (codepoint <= CODEPOINT_HIGHEST_SURROGATE_HALF)) {
-    return -1;
-  }
-
-  *codepointp = codepoint;
-  return len;
-}
-
-static int strcpy_utf16(const char *s, uint16_t *buf, int bufsize) {
-  int i = 0;
-  int buflen = 0;
-
-  while (s[i] != 0) {
-    uint32_t codepoint;
-    int8_t utf8len = utf8Codepoint((const uint8_t *)s + i, &codepoint);
-
-    if (utf8len < 0) {
-      // Invalid utf8 sequence, skip it
-      i++;
-      continue;
-    }
-
-    i += utf8len;
-
-    if (codepoint <= 0xffff) {
-      if (buflen == bufsize)
-        break;
-
-      buf[buflen++] = codepoint;
-
-    } else {
-      if (buflen + 1 >= bufsize)
-        break;
-
-      // Surrogate pair
-      codepoint -= 0x10000;
-      buf[buflen++] = (codepoint >> 10) + 0xd800;
-      buf[buflen++] = (codepoint & 0x3ff) + 0xdc00;
-    }
-  }
-
-  return buflen;
-}
-
-// TODO just for compiling, will move to DFU specific file
-#if CFG_TUD_DFU_RUNTIME && !defined(ARDUINO_ARCH_ESP32)
-void tud_dfu_runtime_reboot_to_dfu_cb(void) {
-  // TinyUSB_Port_EnterDFU();
-}
-#endif
 
 #endif // CFG_TUD_ENABLED
